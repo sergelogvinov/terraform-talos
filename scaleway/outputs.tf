ca0989667429ac93d8ab8b9b894d6b89070e412a
Serge Logvinov <serge.logvinov@sinextra.dev>
2022-06-25 16:33:02 +0300

    Show all contolplane public ips

--- scaleway/outputs.tf
+++ scaleway/outputs.tf
@@ -4,7 +4,7 @@ output "controlplane_endpoint" {
   value       = try(scaleway_lb_ip.lb[0].ip_address, "127.0.0.1")
 }
 
-output "controlplane_firstnode" {
+output "controlplanes" {
   description = "Kubernetes controlplane first node"
-  value       = try(scaleway_instance_ip.controlplane[0].address, "none")
+  value       = try(flatten([for c in scaleway_instance_ip.controlplane : c.address]), "none")
 }
