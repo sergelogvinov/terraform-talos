11c8d850762717d7ecd0f16c53592f3fe7d300b5
Allen <allenjhuang92@gmail.com>
2025-02-11 18:29:53 -0600

    add defaults and descriptions

--- oracle/init/variables.tf
+++ oracle/init/variables.tf
@@ -3,8 +3,14 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_api_key_file" {}
-variable "private_tf_key_file" {}
-variable "public_tf_key_file" {}
+variable "private_tf_key_file" {
+  description = "Filepath to write new private key"
+  default     = "~/.oci/oci_main_terraform.pem"
+}
+variable "public_tf_key_file" {
+  description = "Filepath to write new public key"
+  default     = "~/.oci/oci_main_terraform_public.pem"
+}
 variable "region" {
   description = "the OCI region where resources will be created"
   type        = string
