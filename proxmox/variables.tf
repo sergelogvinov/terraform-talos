d10d9ec4012facb51587446cf605f7432092b9cf
Serge Logvinov <serge.logvinov@sinextra.dev>
2025-09-05 17:21:21 +0700

    update images

--- proxmox/variables.tf
+++ proxmox/variables.tf
@@ -26,7 +26,7 @@ variable "vpc_main_cidr" {
 variable "release" {
   type        = string
   description = "The version of the Talos image"
-  default     = "1.10.3"
+  default     = "1.10.7"
 }
 
 data "sops_file" "tfvars" {
